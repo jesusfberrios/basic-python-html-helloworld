5a967ccf836f9234700ad8cf52cd24a64111e911
Jesus Berrios <81935591+jesusfberrios@users.noreply.github.com>
2024-03-20 19:00:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/hello')
+def home():
+    return 'Hello World!'
+
 if __name__ == '__main__':
     app.run()
